91b81d9a3eb21416c7ca5c56f61c78f96a0c15e0
cloud <cloud@weststarinc.co>
2016-08-02 17:05:09 +0800

    清理

--- src/main/java/com/template/common/Log.java
+++ src/main/java/com/template/common/Log.java
@@ -1,16 +1,18 @@
 package com.template.common;
 
+import org.apache.log4j.Logger;
+
 /**
  * 这个类主要测试一下Apache Log4j日志的用法
  * Created by Cloud on 2016/6/27.
  */
 public class Log {
-//    private static Logger log = Logger.getLogger(Log.class);
+    private static Logger log = Logger.getLogger(Log.class);
 
     public static void main(String[] args) {
-//        log.info("this is info message.");
-//        log.debug("this is debug message.");
-//        log.error("this is error message.", new Exception("这是Exception类中的错误信息!"));
-//        log.fatal("this is fatal message.", new Exception("这是Exception类中的错误信息!"));
+        log.info("this is info message.");
+        log.debug("this is debug message.");
+        log.error("this is error message.", new Exception("这是Exception类中的错误信息!"));
+        log.fatal("this is fatal message.", new Exception("这是Exception类中的错误信息!"));
     }
 }
